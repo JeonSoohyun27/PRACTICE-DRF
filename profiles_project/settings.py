383de51bb4d1318707ce6084258024983974fcf0
JeonSoohyun27 <jeonsoohyun27@gmail.com>
2021-10-28 02:06:20 +0900

    Remove my_settings.py

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
-from my_settings import SECRET_KEY
-
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY
+SECRET_KEY = 'django-insecure-=jy=62$8!!nya)_o6zk0s*c$v24#)#!fptpk%de264^uwb)d!u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG',1)))
