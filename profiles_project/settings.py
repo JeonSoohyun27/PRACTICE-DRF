a7c11dddeb0a0733e5b5643139b5035aec0ee44e
JeonSoohyun27 <jeonsoohyun27@gmail.com>
2021-10-28 01:54:20 +0900

    Configured for deployment

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 from my_settings import SECRET_KEY
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG',1)))
 
 ALLOWED_HOSTS = []
 
@@ -129,3 +131,5 @@ AUTH_USER_MODEL = 'profiles_api.UserProfile'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = 'static/'
