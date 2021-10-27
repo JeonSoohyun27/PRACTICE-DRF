314439026c3e6521cf4dae42ee7bc51e1fc3c4ed
JeonSoohyun27 <jeonsoohyun27@gmail.com>
2021-10-28 02:02:56 +0900

    Fix the setup.sh in deploy

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -3,7 +3,7 @@
 set -e
 
 # TODO: Set to URL of git repo.
-PROJECT_GIT_URL='https://github.com:JeonSoohyun27/profiles-rest-api.git'
+PROJECT_GIT_URL='https://github.com/JeonSoohyun27/profiles-rest-api.git'
 
 PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
